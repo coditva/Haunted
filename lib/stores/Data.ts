d0017105cb3009562d1c1a04edfd7ea1c78b20a3
Utkarsh Maheshwari <coditva@gmail.com>
2021-06-13 00:20:53 +0530

    Store environment variables in config

--- lib/stores/Data.ts
+++ lib/stores/Data.ts
@@ -1,9 +1,8 @@
-const BASE_URL = process.env.GHOST_URL || 'example.com';
-const KEY = process.env.CONTENT_KEY || 'content-key';
+import config from 'config';
 
 export default class Data {
   static async fetch(path: string) {
-    const url = `https://${BASE_URL}/ghost/api/v3${path}?key=${KEY}`;
+    const url = `https://${config.ghost.baseUrl}/ghost/api/v3${path}?key=${config.ghost.contentKey}`;
     const response = await fetch(url);
 
     return response.json();
