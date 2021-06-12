aa4914afb8e3d0960f5f73798b99dbcba551e143
Utkarsh Maheshwari <coditva@gmail.com>
2021-06-12 22:53:35 +0530

    Remove redundant component

--- lib/components/PageContent.tsx
+++ lib/components/PageContent.tsx
@@ -2,7 +2,7 @@ import PageComponent from 'lib/components/Page';
 
 import type Page from 'lib/stores/Page';
 
-export default function PageContainer({ page }: { page: Page }) {
+export default function PageContent({ page }: { page: Page }) {
   return (
     <PageComponent
       title={page.title}
