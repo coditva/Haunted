07961452a4c679d62ebf8bd6bf8c861add7e7434
Utkarsh Maheshwari <coditva@gmail.com>
2021-06-13 18:37:32 +0530

    Add en-IN as locale for the site

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 module.exports = {
+  i18n: {
+    locales: ['en-IN'],
+    defaultLocale: 'en-IN',
+  },
   images: {
     domains: imageDomains,
   },
