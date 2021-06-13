e69a168fd3043ddd4027cb553afcdf13ea47f975
Utkarsh Maheshwari <coditva@gmail.com>
2021-06-13 19:11:12 +0530

    Fix contrast issues with Header navigation links

--- lib/components/Header/Navigation.tsx
+++ lib/components/Header/Navigation.tsx
@@ -4,7 +4,7 @@ import type NavigationItem from 'lib/stores/NavigationItem';
 
 export default function Navigation({ items }: { items: Array<NavigationItem> }) {
   return (
-    <div className='flex flex-wrap font-mono text-gray-500 mt-6 sm:mt-0 space-x-4'>
+    <div className='flex flex-wrap font-mono text-gray-500 dark:text-gray-400 mt-6 sm:mt-0 space-x-4'>
       {
         items.map((item) => (
           <span key={item.label} className='flex-1 hover:underline whitespace-pre'>
